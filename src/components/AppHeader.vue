6eefe716e4d9cfe2b863dd5748048d3520226b19
Alejandro Cruz Bravo <alexcruzbravo1607@gmail.com>
2025-05-27 21:03:26 -0500

    Subiendo proyecto

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1><span class="icon" aria-hidden="true">✅</span> To do</h1>
+    <p>Building Real-world Web Applications with Vue.js 3</p>
+    <p>Practica de Framework para Front-End</p>
+    <p>Hecho por: <strong>Alejandro Cruz Bravo</strong></p>
+  </header>
+</template>
+
+<style scoped>
+header {
+  border-bottom: #181818 1px solid;
+  background-color: #181818;
+}
+header::after {
+  content: '';
+  display: block;
+  height: 1px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+}
+h1 {
+  font-size: 3rem;
+}
+h1 .icon {
+  font-size: 3rem;
+  vertical-align: middle;
+}
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+defineProps<{
+  isChecked?: boolean | false
+}>()
+
+const emit = defineEmits(['update:isChecked'])
+
+const handleChange = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emit('update:isChecked', target.checked)
+}
+</script>
+<template>
+  <label :class="{ checked: isChecked }">
+    <input type="checkbox" :checked="isChecked" @change="handleChange" />
+    <slot></slot>
+  </label>
+</template>
+<style scoped>
+label {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+.checked {
+  text-decoration: line-through;
+  color: #666;
+}
+</style>
